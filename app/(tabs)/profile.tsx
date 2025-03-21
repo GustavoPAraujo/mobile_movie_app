5f0c4721dc7383b464b87b0afb6176bd4eda8a24
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-03-20 22:31:19 -0300

    fix serachbar props  from home

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { images } from '@/constants/images'
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className='flex-1 bg-primary'>
+      <Image source={images.bg} className='absolute w-full z-0' />
+      
     </View>
   )
 }

e581bbd92511d7519aecc1f54b93ed1dae3667d4
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-03-26 14:24:32 -0300

    save and profile pages

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,14 +1,16 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import { images } from '@/constants/images'
+import { icons } from "@/constants/icons";
+import { View, Text, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Profile = () => {
   return (
-    <View className='flex-1 bg-primary'>
-      <Image source={images.bg} className='absolute w-full z-0' />
-      
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary flex-1 px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.person} className="size-10" tintColor="#fff" />
+        <Text className="text-gray-500 text-base">Profile</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Profile
+export default Profile;
